de7b5d677c5af1569690f54b7169288090bd6ccb
Raj Patel <rajp.patel@thegatewaycorp.com>
2021-06-09 15:43:44 +0530

    Enhancements in extractor file

--- extractor.py
+++ extractor.py
@@ -1,20 +1,25 @@
 import PyPDF2
 
-# give the path of your pdf file
-file_obj = open('input/RP2.pdf', 'rb')
-filereader=PyPDF2.PdfFileReader(file_obj)
+def pdf_to_text(file):
+    # give the path of your pdf file
+    file_obj = open(file, 'rb')
+    filereader=PyPDF2.PdfFileReader(file_obj)
 
-total_pages = filereader.numPages
-count = 0
-str = ""
+    total_pages = filereader.numPages
+    count = 0
+    str = ""
 
-# the while loop will read each page
-# extractText() will extract the text of each page from PDF
-while count < total_pages:
-    str += filereader.getPage(count).extractText()
-    count += 1
+    # the while loop will read each page
+    # extractText() will extract the text of each page from PDF
+    while count < total_pages:
+        str += filereader.getPage(count).extractText() + "\n" + "**End Page**" + "\n"
+        count += 1
 
-# the following line will generate the output file
-with open("output_RP2.txt", "w") as f:
-    f.write(str)
-    f.close()
+    # the following line will generate the output file
+    with open("output_RP2.txt", "w") as f:
+        f.write(str)
+        f.close()
+
+if __name__ == '__main__':
+    file_path = 'input/RP2.pdf'
+    pdf_to_text(file_path)
