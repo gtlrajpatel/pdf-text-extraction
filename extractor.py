d8cbda89a3e09f059c4e02f851e57d8ddf001aff
Raj Patel <rajp.patel@thegatewaycorp.com>
2021-06-11 19:27:16 +0530

    Add a function to extract text from files using tika

--- extractor.py
+++ extractor.py
@@ -1,10 +1,15 @@
-import PyPDF2
 import os
+import PyPDF2
+
+from tika import parser
 
 def pdf_to_text(file):
+    """
+    This function uses PyPDF2 library to extract text from PDF files
+    """
     # give the path of your pdf file
     file_obj = open(file, 'rb')
-    filereader=PyPDF2.PdfFileReader(file_obj)
+    filereader=PyPDF2.PdfFileReader(file_obj, strict=False)
 
     total_pages = filereader.numPages
     count = 0
@@ -23,8 +28,22 @@ def pdf_to_text(file):
         f.write(str)
         f.close()
 
+def text_extraction_using_tika(file):
+    """
+    This function uses Tika library to extract text from files
+    """
+    parsed_file = parser.from_file(file)
+    data = parsed_file['content']
+
+    filename = os.path.splitext(os.path.basename(file))[0]
+
+    with open("output/"+ filename + "_tika.txt", "w") as f:
+        f.write(data)
+        f.close()
+
+
 if __name__ == '__main__':
     input_files_directory = 'input'
     for filename in os.listdir(input_files_directory):
-        print("\n\n ----->>>>", filename)
         pdf_to_text('input/'+filename)
+        text_extraction_using_tika('input/'+filename)
