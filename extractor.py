8dd83278cdf298013367a571bacd7342e55905b0
Raj Patel <rajp.patel@thegatewaycorp.com>
2021-06-17 18:31:03 +0530

    Refactor the text extraction code

--- extractor.py
+++ extractor.py
@@ -3,13 +3,14 @@ import PyPDF2
 
 from tika import parser
 
+
 def pdf_to_text(file):
     """
     This function uses PyPDF2 library to extract text from PDF files
     """
     # give the path of your pdf file
     file_obj = open(file, 'rb')
-    filereader=PyPDF2.PdfFileReader(file_obj, strict=False)
+    filereader = PyPDF2.PdfFileReader(file_obj, strict=False)
 
     total_pages = filereader.numPages
     count = 0
@@ -24,26 +25,42 @@ def pdf_to_text(file):
     filename = os.path.splitext(os.path.basename(file))[0]
 
     # the following line will generate the output file
-    with open("output/"+ filename + ".txt", "w") as f:
+    with open("output/" + filename + ".txt", "w") as f:
         f.write(str)
         f.close()
 
-def text_extraction_using_tika(file):
-    """
-    This function uses Tika library to extract text from files
-    """
-    parsed_file = parser.from_file(file)
-    data = parsed_file['content']
 
-    filename = os.path.splitext(os.path.basename(file))[0]
+def text_extraction_using_tika(input_directory, output_directory):
+    """
+    This function uses Tika library to extract text from PDF, PPT, HTML & DOC files
+    """
+    pdf_count, doc_count, ppt_count, html_count = 0, 0, 0, 0
+    for filename in os.listdir(input_directory):
+        parsed_file = parser.from_file(input_directory + '/' + filename)
+        data = parsed_file['content']
 
-    with open("output/"+ filename + "_tika.txt", "w") as f:
-        f.write(data)
-        f.close()
+        filename, ext = os.path.splitext(os.path.basename(filename))
+
+        try:
+            with open(output_directory + '/' + filename + "_using_tika.txt", "w") as f:
+                f.write(data)
+            if ext == '.pdf':
+                pdf_count += 1
+            if ext == '.docx' or ext == '.doc':
+                doc_count += 1
+            if ext == '.pptx' or ext == '.ppt':
+                ppt_count += 1
+            if ext == '.html':
+                html_count += 1
+        except Exception as e:
+            print("\n\n Writing to file %s failed due to: %s", filename, e)
+
+    print("\n\n> Processed PDF Files: %s \n> Processed DOC Files: %s \n> "
+          "Processed PPT Files: %s \n> Processed HTML Files: %s \n" % (pdf_count, doc_count, ppt_count, html_count))
 
 
 if __name__ == '__main__':
-    input_files_directory = 'input'
-    for filename in os.listdir(input_files_directory):
-        pdf_to_text('input/'+filename)
-        text_extraction_using_tika('input/'+filename)
+    print("\n\n\n *** Text Extraction Program *** \n\n")
+    input_file_directory = input("> Enter the input directory path:")
+    output_file_directory = input("> Enter the output directory path:")
+    text_extraction_using_tika(input_file_directory, output_file_directory)
